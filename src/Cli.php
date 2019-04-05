e229bfc1093057dd9739271e53f7596303fc43bb
Anatoly <abrekov@ya.ru>
2019-04-05 20:30:27 +0400

    change architecture

--- src/Cli.php
+++ src/Cli.php
@@ -1,15 +1,31 @@
 <?php
 
 namespace BrainGames\Cli;
-
+use BrainGames\games\even;
 use function \cli\line;
 use function \cli\prompt as getName;
 use function \cli\prompt as getAnswer;
 
-
-function run()
+const ROUNDSOFGAME = 3;
+function run($gameDescription, $questionAndCorrectAnswer)
 {
     line('Welcome to the Brain Games!');
+    line($gameDescription);
     $name = getName('May I have your name?');
     line("Hello, %s!", $name);
-};
+
+    for ($i = 0; $i < ROUNDSOFGAME; $i++) {
+        [$question, $correctAnswer] = $questionAndCorrectAnswer();
+        line("Question: {$question}");
+        $answer = getAnswer('Your answer');
+
+        if ($answer !== $correctAnswer) {
+            line("{$answer} is wrong answer ;(. Correct answer was {$correctAnswer}.");
+            line("Let's try again, {$name}!");
+            return;
+        } else {
+            line('Correct!');
+        }
+    }
+    line("Congratulations, {$name}!");
+}
--- src/games/even.php
+++ src/games/even.php
@@ -1,39 +1,22 @@
 <?php
 namespace BrainGames\games\even;
 
-use function \cli\line;
-use function \cli\prompt as getName;
-use function \cli\prompt as getAnswer;
+use function BrainGames\Cli\run;
 
 function isEven(int $number)
 {
     return $number % 2 === 0;
 }
 
+const GAMEDESCRIPTION = 'Answer "yes" if number even otherwise answer "no".';
 
-function run()
+function gameEven()
 {
-    $roundsOfGame = 3;
-    $gameDescription = 'Answer "yes" if number even otherwise answer "no".';
-
-    line('Welcome to the Brain Games!');
-    line($gameDescription);
-    $name = getName('May I have your name?');
-    line("Hello, %s!", $name);
-
-    for ($i = 0; $i < $roundsOfGame; $i++) {
+    $questionAndCorrectanswer = function () {
         $question = rand(1, 100);
         $correctAnswer = isEven($question) ? 'yes' : 'no';
-        line("Question: {$question}");
-        $answer = getAnswer('Your answer');
+        return [$question, $correctAnswer];
+    };
 
-        if ($answer !== $correctAnswer) {
-            line("{$answer} is wrong answer ;(. Correct answer was {$correctAnswer}.");
-            line("Let's try again, {$name}!");
-            return;
-        } else {
-            line('Correct!');
-        }
-    }
-    line("Congratulations, {$name}!");
-};
+    run(GAMEDESCRIPTION, $questionAndCorrectanswer);
+}
