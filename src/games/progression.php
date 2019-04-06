5f50f672f45033a2f5302cd5e3cd3f54f6de4bad
Anatoly <abrekov@ya.ru>
2019-04-06 20:55:11 +0400

    change function

--- src/games/progression.php
+++ src/games/progression.php
@@ -10,23 +10,39 @@ function playProgression()
     $generateQuestionAndCorrectanswer = function () {
         $beginOfSequence = rand(1, 100);
         $step = 2;
-        $correctAnswer = rand(0, LENGTH - 1) * $step + $beginOfSequence;
-        $question = getQuestion($beginOfSequence, $step, LENGTH, $correctAnswer);
-        $correctAnswer = (string) $correctAnswer;
+        $endOfSequence = $beginOfSequence + $step * (LENGTH - 1);
+        $sequence = range($beginOfSequence, $endOfSequence, $step);
+        $key = array_rand($sequence);
+        $question = implode(' ', array_replace($sequence, [$key => '..']));
+        $correctAnswer = (string) $sequence[$key];
         return [$question, $correctAnswer];
     };
     run(GAME_DESCRIPTION, $generateQuestionAndCorrectanswer);
 }
 
-function getQuestion($beginOfSequence, $step, $length, $correctAnswer)
-{
-    $endOfSequence = $beginOfSequence + $length * $step;
-    for ($i = $beginOfSequence; $i < $endOfSequence; $i += $step) {
-        if ($i === $correctAnswer) {
-            $result = "{$result} ..";
-        } else {
-            $result = "{$result} {$i}";
-        }
-    }
-    return $result;
-}
+
+// function playProgression()
+// {
+//     $generateQuestionAndCorrectanswer = function () {
+//         $beginOfSequence = rand(1, 100);
+//         $step = 2;
+//         $correctAnswer = rand(0, LENGTH - 1) * $step + $beginOfSequence;
+//         $question = getQuestion($beginOfSequence, $step, LENGTH, $correctAnswer);
+//         $correctAnswer = (string) $correctAnswer;
+//         return [$question, $correctAnswer];
+//     };
+//     run(GAME_DESCRIPTION, $generateQuestionAndCorrectanswer);
+// }
+
+// function getQuestion($beginOfSequence, $step, $length, $correctAnswer)
+// {
+//     $endOfSequence = $beginOfSequence + $length * $step;
+//     for ($i = $beginOfSequence; $i < $endOfSequence; $i += $step) {
+//         if ($i === $correctAnswer) {
+//             $result = "{$result} ..";
+//         } else {
+//             $result = "{$result} {$i}";
+//         }
+//     }
+//     return $result;
+// }
